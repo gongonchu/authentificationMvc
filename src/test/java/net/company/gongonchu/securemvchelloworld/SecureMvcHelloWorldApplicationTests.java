45d7c83d0a62ff2be0d01aeeeef423ee6e741384
gtombette <gontran.tombette@soprasteria.com>
2024-01-12 15:37:33 +0100

    Authentification

---
+++ src/test/java/net/company/gongonchu/securemvchelloworld/SecureMvcHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.company.gongonchu.securemvchelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMvcHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
